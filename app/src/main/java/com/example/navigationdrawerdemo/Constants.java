3ce0210c46e102173be44ab408e8e6434140400b
Itsmejain <shubhamjain.ofcl@gmail.com>
2022-02-21 14:24:53 +0530

    Contact_us - COMPLETE

---
+++ app/src/main/java/com/example/navigationdrawerdemo/Constants.java
@@ -0,0 +1,6 @@
+package com.example.navigationdrawerdemo;
+
+public class Constants {
+    public static String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    public static String CCB_WEBSITE_LINK = "https://ccb.iiitd.ac.in/";
+}
