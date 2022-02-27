74402f0d3c94d16d110ec85ef58d3a3ca5b69621
kuldeep <ksksingh022@gmail.com>
2022-02-27 13:19:09 +0530

    tab layout added in search fragment

--- app/src/main/java/com/example/navigationdrawerdemo/SearchFragment.java
+++ app/src/main/java/com/example/navigationdrawerdemo/SearchFragment.java
@@ -5,15 +5,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class SearchFragment extends Fragment {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_search,container,false);
+        tabLayout = v.findViewById(R.id.tabLayout);
+        viewPager = v.findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+        ViewPageAdapter viewPageAdapter = new ViewPageAdapter(getActivity().getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPageAdapter.addFragments(new CuisineSearchFragment(),"Cuisine Search");
+        viewPageAdapter.addFragments(new IngredientSearchFragment(),"Ingredient Search");
+        viewPageAdapter.addFragments(new CategorySearchFragment(),"Category Search");
+        viewPageAdapter.addFragments(new NutritionSearchFragment(),"Nutrition Search");
+        viewPageAdapter.addFragments(new AdvancedSearchFragment(),"Advanced Search");
+        viewPager.setAdapter(viewPageAdapter);
         return v;
     }
+
 }
