1c2303b216cd2d1dcb7d873605e3f4a655520c6a
Itsmejain <shubhamjain.ofcl@gmail.com>
2022-03-11 00:41:12 +0530

    recipe of the day image API

--- app/src/main/java/com/example/navigationdrawerdemo/RecipeDetailActivity.java
+++ app/src/main/java/com/example/navigationdrawerdemo/RecipeDetailActivity.java
@@ -2,7 +2,9 @@ package com.example.navigationdrawerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
@@ -10,7 +12,18 @@ public class RecipeDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+        Intent intent = getIntent();
+        String requestType = intent.getStringExtra("REQUEST_TYPE");
+        Log.d(Constants.TAG, "onCreate: REQUEST TYPE :"+requestType);
+        RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
+        Bundle bundle = new Bundle();
+        if(requestType.equalsIgnoreCase("RECIPEOFTHEDAY")){
+            bundle.putString("REQUEST_TYPE","RECIPEOFTHEDAY");
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.container,new RecipeDetailFragment()).commit();
+        }else{
+            bundle.putString("REQUEST_TYPE","NORMAL_REQUEST");
+        }
+        recipeDetailFragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,recipeDetailFragment).commit();
     }
 }
