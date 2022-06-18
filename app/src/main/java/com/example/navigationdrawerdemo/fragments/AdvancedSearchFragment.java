8fc76588ecaf34678a925bea11ad6a609d74f2af
kuldeep <ksksingh022@gmail.com>
2022-06-18 23:54:04 +0530

    all dropdowns added

--- app/src/main/java/com/example/navigationdrawerdemo/fragments/AdvancedSearchFragment.java
+++ app/src/main/java/com/example/navigationdrawerdemo/fragments/AdvancedSearchFragment.java
@@ -7,21 +7,99 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 import com.example.navigationdrawerdemo.R;
+import com.example.navigationdrawerdemo.util.Constants;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdvancedSearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AdvancedSearchFragment extends Fragment {
 
-
+    ArrayAdapter<String> regionAdapterItems;
+    ArrayAdapter<String> countryAdapterItems;
+    ArrayAdapter<String> continentAdapterItems;
+    ArrayAdapter<String> utensilsAdapterItems;
+    ArrayAdapter<String> processAdapterItems;
+    AutoCompleteTextView country ;
+    AutoCompleteTextView region;
+    AutoCompleteTextView continent;
+    AutoCompleteTextView utensil;
+    AutoCompleteTextView process;
+    String[] regions = Constants.REGIONS;
+    String[] countries = Constants.COUNTRIES;
+    String[] continents = Constants.CONTINENTS;
+    String [] utensils = Constants.UTENSIL_NAMES;
+    String [] processes = Constants.PROCESS_NAMES;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_advanced_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_advanced_search, container, false);
+        continent = v.findViewById(R.id.advcontinentEditBox);
+        country = v.findViewById(R.id.advCountryEditBox);
+        region = v.findViewById(R.id.advRegionEditBox);
+        utensil = v.findViewById(R.id.utensilEditBox);
+        process = v.findViewById(R.id.cookingEditBox);
+
+
+        regionAdapterItems = new ArrayAdapter<String>(getContext(),R.layout.region_list,regions);
+        countryAdapterItems = new ArrayAdapter<String>(getContext(),R.layout.region_list,countries);
+        continentAdapterItems = new ArrayAdapter<String>(getContext(),R.layout.region_list,continents);
+        utensilsAdapterItems = new ArrayAdapter<String>(getContext(),R.layout.region_list,utensils);
+        processAdapterItems = new ArrayAdapter<String>(getContext(),R.layout.region_list,processes);
+        region.setAdapter(regionAdapterItems);
+        country.setAdapter(countryAdapterItems);
+        continent.setAdapter(continentAdapterItems);
+        utensil.setAdapter(utensilsAdapterItems);
+        process.setAdapter(processAdapterItems);
+
+        utensil.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String utensilVal = utensil.getText().toString();
+                Toast.makeText(getContext(), "region: "+utensilVal, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        process.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String processVal = process.getText().toString();
+                Toast.makeText(getContext(), "region: "+processVal, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        region.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String regionVal = region.getText().toString();
+                Toast.makeText(getContext(), "region: "+regionVal, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        country.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String countryVal = country.getText().toString();
+                Toast.makeText(getContext(), "region: "+countryVal, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        continent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String continentVal = continent.getText().toString();
+                Toast.makeText(getContext(), "region: "+continentVal, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return v;
     }
 }
--- app/src/main/java/com/example/navigationdrawerdemo/fragments/CategorySearchFragment.java
+++ app/src/main/java/com/example/navigationdrawerdemo/fragments/CategorySearchFragment.java
@@ -7,21 +7,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.navigationdrawerdemo.R;
+import com.example.navigationdrawerdemo.util.Constants;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CategorySearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CategorySearchFragment extends Fragment {
 
-
+    Button searchButton;
+    AutoCompleteTextView ingredientUsed;
+    AutoCompleteTextView ingredientNotUsed;
+    String [] ingredientCategories = Constants.INGREDIENT_CATEGORIES;
+    ArrayAdapter<String> ingredientAdapterItems;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_category_search, container, false);
+        searchButton = v.findViewById(R.id.searchByCategory);
+        ingredientUsed = v.findViewById(R.id.usedCategoryEditBox);
+        ingredientNotUsed = v.findViewById(R.id.notUsedCategoryEditBox);
+        ingredientAdapterItems = new ArrayAdapter<String>(getContext(),R.layout.region_list,ingredientCategories);
+        ingredientUsed.setAdapter(ingredientAdapterItems);
+        ingredientUsed.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String ingredientVal = ingredientUsed.getText().toString();
+                Toast.makeText(getContext(), "$ingredientVal", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ingredientNotUsed.setAdapter(ingredientAdapterItems);
+        ingredientNotUsed.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                String ingredientVal = ingredientUsed.getText().toString();
+                Toast.makeText(getContext(), "$ingredientVal", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return v;
     }
 }
